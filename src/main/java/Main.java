7232ed311a0c541f203730647a655e7a54ac06db
Nikita Kokovin <kokovinnikita1606@gmail.com>
2025-10-15 23:22:15 +0300

    New program Operations with numbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner (System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner (System.in).nextInt();
+        int sumNumber = firstNumber + secondNumber;
+        int difNumber = firstNumber - secondNumber;
+        int prodNumber = firstNumber * secondNumber;
+        double  quotNumber = (double)firstNumber/secondNumber;
+        System.out.println("Сумма чисел: " + sumNumber);
+        System.out.println("Разность чисел: " + difNumber);
+        System.out.println("Произведение чисел: " + prodNumber);
+        System.out.println("Частное чисел: " + quotNumber);
     }
 }
