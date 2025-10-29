beb3be6ee6cbc2390f9732eb5660265df86a73d1
Nikita Kokovin <kokovinnikita1606@gmail.com>
2025-10-29 21:39:09 +0300

    checking the file path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,28 @@
+package main.java;
+
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число:");
-        int firstNumber = new Scanner (System.in).nextInt();
-        System.out.println("Введите второе число:");
-        int secondNumber = new Scanner (System.in).nextInt();
-        int sumNumber = firstNumber + secondNumber;
-        int difNumber = firstNumber - secondNumber;
-        int prodNumber = firstNumber * secondNumber;
-        double  quotNumber = (double)firstNumber/secondNumber;
-        System.out.println("Сумма чисел: " + sumNumber);
-        System.out.println("Разность чисел: " + difNumber);
-        System.out.println("Произведение чисел: " + prodNumber);
-        System.out.println("Частное чисел: " + quotNumber);
+        //Счетчик количество верно указанных путей к файлам
+        int countTrueSelectPathsFiles = 0;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExists || isDirectory) {
+                if (!fileExists) {
+                    System.out.println("Ошибка: указанный файл не существует");
+                } else {
+                    System.out.println("Ошибка: указанный путь является путём к папке");
+                } continue;
+            } else {
+                countTrueSelectPathsFiles++;
+                System.out.println("Путь указан верно!");
+                System.out.println("Это файл номер " + countTrueSelectPathsFiles);
+            }
+        }
     }
 }
