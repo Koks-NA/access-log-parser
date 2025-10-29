0c95085e5757d4af900cd7decc5010f893ea2364
Nikita Kokovin <kokovinnikita1606@gmail.com>
2025-10-29 21:52:57 +0300

    checking the file path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        //Счетчик количество верно указанных путей к файлам
+        int countTrueSelectPathsFiles = 0;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExists || isDirectory) {
+                if (!fileExists) {
+                    System.out.println("Ошибка: указанный файл не существует");
+                } else {
+                    System.out.println("Ошибка: указанный путь является путём к папке");
+                } continue;
+            } else {
+                countTrueSelectPathsFiles++;
+                System.out.println("Путь указан верно!");
+                System.out.println("Это файл номер " + countTrueSelectPathsFiles);
+            }
+        }
     }
 }
