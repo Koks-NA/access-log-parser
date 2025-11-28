3ba362b216323f2955230ae2baf2ac27f3f32dcf
Nikita Kokovin <kokovinnikita1606@gmail.com>
2025-11-28 11:12:42 +0300

    add logic Exception handli

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
@@ -19,6 +21,34 @@ public class Main {
             } else {
                 countTrueSelectPathsFiles++;
                 System.out.println("Путь указан верно!");
+                try {
+                    FileReader fileReader = new FileReader(path);
+                    BufferedReader reader = new BufferedReader(fileReader);
+                    String line;
+                    int totalLines=0;
+                    int maxLength=0;
+                    int minLength=Integer.MAX_VALUE;
+                    while ((line = reader.readLine()) != null) {
+                        int length = line.length();
+                        totalLines++;//считаем строки
+                        if(length>maxLength){
+                            maxLength=length; //ищем максимальную длину строки
+                        }
+                        if(length<minLength){
+                            minLength=length; //ищем минимальную длину строки
+                        }
+                        if (length>1024){
+                            throw new LongLineException ("Строка слишком длинная: " + length + " символов");
+                        }
+                    }
+                    System.out.println("Количество строк в файле: "+totalLines);
+                    System.out.println("Максимальная длина строки: "+maxLength);
+                    System.out.println("Минимальная длина строки: "+minLength);
+
+                    reader.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
                 System.out.println("Это файл номер " + countTrueSelectPathsFiles);
             }
         }
